ab48c7f262a39754d1f856a75909440de103234f
campishome <campnapat007@gmail.com>
2024-09-14 01:20:41 +0700

    fixed

--- index.js
+++ index.js
@@ -3,25 +3,40 @@ const mysql = require('mysql');
 const app = express();
 const PORT = 4000;
 
-const db = mysql.createConnection({
-  host: '47.129.222.47',
-  user: 'camp',
-  password: 'camp',
-  database: 'mobile_project',
-  port: 3306
-});
-
-db.connect((err) => {
-  if (err) {
-    console.error('Error connecting to the database:', err);
-    return;
-  }
-  console.log('Connected to the database.');
-
-  // เริ่มฟังที่พอร์ตหลังจากการเชื่อมต่อฐานข้อมูลสำเร็จ
-  app.listen(PORT, () => {
-    console.log(`API Listening on PORT ${PORT}`);
+// ฟังก์ชันสำหรับสร้างการเชื่อมต่อฐานข้อมูลใหม่
+const createDbConnection = () => {
+  const db = mysql.createConnection({
+    host: '47.129.222.47',
+    user: 'camp',
+    password: 'camp',
+    database: 'mobile_project',
+    port: 3306
   });
+
+  db.connect((err) => {
+    if (err) {
+      console.error('Error connecting to the database:', err);
+      return;
+    }
+    console.log('Connected to the database.');
+  });
+
+  // ตรวจสอบการเชื่อมต่อ
+  db.on('error', (err) => {
+    console.error('Database error:', err);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      // การเชื่อมต่อถูกตัดการเชื่อมต่อให้ลองเชื่อมต่อใหม่
+      createDbConnection();
+    }
+  });
+
+  return db;
+};
+
+const db = createDbConnection();
+
+app.listen(PORT, () => {
+  console.log(`API Listening on PORT ${PORT}`);
 });
 
 app.get('/', (req, res) => {
@@ -33,13 +48,13 @@ app.get('/test', (req, res) => {
 });
 
 app.get('/connect', (req, res) => {
-  const query = 'SELECT * FROM LottoResult';  // ใช้ตารางที่คุณต้องการ
+  const query = 'SELECT * FROM LottoResult';  
   db.query(query, (error, results) => {
     if (error) {
       console.error('Error executing query:', error);
       res.status(500).send('Error fetching data.');
     } else {
-      res.status(200).json(results);  // ส่งข้อมูลในรูปแบบ JSON
+      res.status(200).json(results);
     }
   });
 });
